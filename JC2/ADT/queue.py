d9a169bcd30f16ed8f5d3f4827aa2cf2abdcd33b
kayleensuryaputra <kayleensuryaputra@gmail.com>
2025-02-05 09:52:47 +0700

    test2

---
+++ JC2/ADT/queue.py
@@ -0,0 +1,52 @@
+Queue = [None for i in range(5)]
+print(Queue)
+Rear = -1
+Front = 0
+lenofqueue = 0
+maxsize =  5
+
+def enqueue(newelement):
+    global lenofqueue
+    global Rear
+    if lenofqueue == maxsize:
+        print("Queue is full")
+    else:
+        if Rear == maxsize - 1:
+            Rear = 0
+        else:
+            Rear = Rear + 1
+        Queue[Rear] = newelement
+        lenofqueue = lenofqueue + 1
+        print(Queue)
+
+            
+def dequeue():
+    global Front
+    global lenofqueue
+    if lenofqueue == 0:
+        print("Queue is empty")
+    else:
+        Queue[Front] = None
+        lenofqueue = lenofqueue - 1
+        print(Queue)
+        if Front ==  maxsize - 1:
+            Front = 0
+        else:
+            Front = Front + 1
+
+
+enqueue(1)
+enqueue(7)
+enqueue(5)
+enqueue(3)
+enqueue(2)
+dequeue()
+enqueue(9) #until here correct
+
+dequeue() 
+dequeue()
+dequeue()
+dequeue()
+
+#isEmpty() & isFull()
+
