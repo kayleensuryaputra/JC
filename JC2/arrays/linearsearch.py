811961fddef51dece7e93f40aadeedea48c0ece0
kayleensuryaputra <kayleensuryaputra@gmail.com>
2025-03-14 21:15:16 +0700

    14/03/2025

--- JC2/arrays/linearsearch.py
+++ JC2/arrays/linearsearch.py
@@ -5,10 +5,14 @@ myArray = [1,2,3,4,5,6]
 
 search = int(input("What value is being searched?")) #assigning a search value
 
-for i in range(len(myArray)):
-    if search == myArray[i]:
-        print("Value found")
-    else:
-        print("Value not found")
+def linearSearch(myArray, search):
+    for i in range(len(myArray)):
+        if search == myArray[i]:
+            return i
+    return -1
 
-#wrong
+result =  linearSearch(myArray, search)
+if result == -1:
+    print ("Value not found")
+else:
+    print (f"Value is found. The index is: {result}")
--- JC2/pyp/9618_s21_qp_42.py
+++ JC2/pyp/9618_s21_qp_42.py
@@ -20,36 +20,92 @@ linkedList = [node(1,1),
               node(0,9),
               node(0,-1)]
 
-#(c)(i)
+# #(c)(i)
 startPointer = 0
 emptyList = 5
-def outputNodes(linkedList, startPointer):
-    index = startPointer
-    while linkedList[index].nextNode != -1:
-        print(linkedList[index].data)
-        index = linkedList[index].nextNode
-    print(linkedList[index].data)
+# def outputNodes(linkedList, startPointer):
+#     index = startPointer
+#     while linkedList[index].nextNode != -1:
+#         print(linkedList[index].data)
+#         index = linkedList[index].nextNode
+#     print(linkedList[index].data)
 
-#(c)(ii)
-outputNodes(linkedList, startPointer)
+# #(c)(ii)
+# outputNodes(linkedList, startPointer)
 
-#(d)(i)
-def addNode(linkedList, startPointer, emptyList):
-    lastindex = startPointer
-    newData = input(int("Input new data: "))
-    linkedList[emptyList] = newData
-
+# #(d)(i)
+# def addNode(linkedList, startPointer, emptyList):
+#     lastindex = startPointer
+#     newData = int(input("Input new data: "))
+#     linkedList[emptyList] = newData
     
-    #tetyfdstyewfdsye
+#     if emptyList != len(linkedList) + 1:
+#         while linkedList[lastindex].nextNode != -1:
+#             lastindex = linkedList[lastindex].nextNode
+#         linkedList[lastindex] = emptyList
+
+#         while emptyList != len(linkedList):
+#             if emptyList != len(linkedList):
+#                 while linkedList[emptyList].nextNode != -1:
+#                     emptyList = emptyList + 1
+#                 return True
+#             else:
+#                 return False
+#     else:
+#         return False
 
 
-# #QUESTION 2
-# #(a)
-# arrayData = [None for i in range (10)]
-# arrayData = [10,5,6,7,1,12,13,15,21,8]
+# print(addNode(linkedList, startPointer, emptyList))
+# print (emptyList)
+
+# #(d)(ii)
+# outputNodes(linkedList, startPointer)
+# result = addNode(linkedList, startPointer, emptyList)
+# if result == False:
+#     print ("Sorry the list is full")
+# else:
+#     print ("Data has been added")
+# outputNodes(linkedList, startPointer)
+
+
+#QUESTION 2
+#(a)
+arrayData = [None for i in range (10)]
+arrayData = [10,5,6,7,1,12,13,15,21,8]
 
 # #(b)(i)
-# def linearSearch(searchEle):
-#     for i in range (len(arrayData)):
-#         if searchEle == arrayData[i]:
-#             return True
+def linearSearch(searchEle):
+    for i in range (len(arrayData)):
+        if searchEle == arrayData[i]:
+            return True
+        else:
+            i = i + 1
+    return False
+
+#(b)(ii)
+searchEle = int(input("Input an element to be searched: "))
+result = linearSearch(searchEle)
+if result == False:
+    print ("Search element not found")
+else:
+    print ("Search element is found")
+
+#(c)
+def bubbleSort():
+    for y in range(0, 10):
+        if arrayData[y] > arrayData[y+1]:
+            temp = arrayData[y]
+            arrayData[y] = arrayData[y+1]
+            arrayData[y+1] = temp
+
+#QUESTION 3
+
+#(a)
+class TreasureChest:
+    def __init__(self, question, answer, points):
+        self.__question  = question
+        self.__answer = answer
+        self.__points = points
+    
+    def getQuestion():
+        
