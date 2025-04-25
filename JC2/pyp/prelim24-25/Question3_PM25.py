a4203e59586a328dfad0dde7f53252e13b0a40a8
kayleensuryaputra <kayleensuryaputra@gmail.com>
2025-04-25 11:24:27 +0700

    25/04/2025.3

--- JC2/pyp/prelim24-25/Question3_PM25.py
+++ JC2/pyp/prelim24-25/Question3_PM25.py
@@ -1,5 +1,27 @@
 
 #(a)(i)
 class Device:
-    def __init__(self,device_name):
+    def __init__(self,device_name,brand,battery_life,price):
         self.device_name = device_name
+        self.brand = brand
+        self.battery_life = battery_life
+        self.price = price
+    
+    def get_device_name(self):
+        return self.device_name
+    
+    def get_brand(self):
+        return self.brand
+    
+    def get_battery_life(self):
+        return self.battery_life
+    
+    def get_price(self):
+        return self.price
+    
+    def print_details(self, device_name, brand, battery_life, price):
+        print(f"Device Name: {device_name}, Brand: {brand}, Battery Life: {battery_life}, Price: {price}")
+
+#(a)(ii)
+class Phone:
+    
